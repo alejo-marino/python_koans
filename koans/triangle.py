c09751fc9f2177c9da4d11b8fd7fdb8f1a5d6d29
alejo-marino <tommy230201@gmail.com>
2024-11-27 15:21:18 -0300

    Completed 13/37 koans

--- koans/triangle.py
+++ koans/triangle.py
@@ -16,9 +16,25 @@
 # and
 #   about_triangle_project_2.py
 #
+"""def triangle(a, b, c):
+    if a == b:
+        if a == c and b == c: return 'equilateral'
+        return 'isosceles'
+    elif b == c:
+        if a == b and a == c: return 'equilateral'
+        return 'isosceles'
+    elif a == c:
+        if a == b and b == c: return 'equilateral'
+        return 'isosceles'
+    return 'scalene'
+"""
+# alternative solution using sets: # Could store a, b and c in a set and check the len of the set to know the amount of unique sides
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    sides = set([a, b, c])
+    if len(sides) == 1: return 'equilateral'
+    elif len(sides) == 2: return 'isosceles'
+    return 'scalene'
+#"""
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
