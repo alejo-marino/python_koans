fe2988110ee8e82fccb03062bc7f9356ed4f9612
alejo-marino <tommy230201@gmail.com>
2024-12-03 18:40:18 +0000

    Completed 35/37 koans

--- koans/about_deleting_objects.py
+++ koans/about_deleting_objects.py
@@ -9,13 +9,13 @@ class AboutDeletingObjects(Koan):
         del lottery_nums[1]
         del lottery_nums[2:4]
 
-        self.assertEqual(___, lottery_nums)
+        self.assertEqual([4, 15, 42], lottery_nums)
 
     def test_del_can_remove_entire_lists(self):
         lottery_nums = [4, 8, 15, 16, 23, 42]
         del lottery_nums
 
-        with self.assertRaises(___): win = lottery_nums
+        with self.assertRaises(UnboundLocalError): win = lottery_nums
 
     # ====================================================================
 
@@ -48,8 +48,8 @@ class AboutDeletingObjects(Koan):
         except AttributeError as e:
             err_msg2 = e.args[0]
 
-        self.assertRegex(err_msg1, __)
-        self.assertRegex(err_msg2, __)
+        self.assertRegex(err_msg1, "'ClosingSale' object has no attribute 'toilet_brushes'")  # shouldn't exist in the class
+        self.assertRegex(err_msg2, "'ClosingSale' object has no attribute 'hamsters'")  # shouldn't exist in this instance of the class
 
     # ====================================================================
 
@@ -60,7 +60,8 @@ class AboutDeletingObjects(Koan):
         def get_name(self):
             try:
                 return self._name
-            except:
+            except: # Exception as e:
+                # raise(e) AttributeError: 'ClintEastwood' object has no attribute '_name'. Did you mean: 'name'
                 return "The man with no name"
 
         def set_name(self, name):
@@ -78,7 +79,7 @@ class AboutDeletingObjects(Koan):
         self.assertEqual('Senor Ninguno', cowboy.name)
 
         del cowboy.name
-        self.assertEqual(__, cowboy.name)
+        self.assertEqual("The man with no name", cowboy.name)
 
 
     # ====================================================================
@@ -105,7 +106,7 @@ class AboutDeletingObjects(Koan):
         self.assertEqual('Patrick', citizen.name)
 
         del citizen.name
-        self.assertEqual(__, citizen.name)
+        self.assertEqual('Number Six', citizen.name)
 
     # ====================================================================
 
@@ -119,6 +120,6 @@ class AboutDeletingObjects(Koan):
 
     def tests_del_can_be_overriden(self):
         sale = self.MoreOrganisedClosingSale()
-        self.assertEqual(__, sale.jellies())
+        self.assertEqual(5, sale.jellies())
         del sale.jellies
-        self.assertEqual(__, sale.last_deletion)
+        self.assertEqual("jellies", sale.last_deletion)
